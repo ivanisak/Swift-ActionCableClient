33bd25b32d80ad2d1e444f626fd690e0c4c62668
Ivan Isak <ivanisak42@gmail.com>
2019-03-28 16:38:39 +0200

    fixes

--- Source/Classes/Channel.swift
+++ Source/Classes/Channel.swift
@@ -53,8 +53,12 @@ open class Channel: Hashable, Equatable {
             var channelUID = name
             
             //if identifier isn't empty, fetch the first value as the channel unique identifier
-            if let dictionary = identifier?.first {
-                channelUID = dictionary.value as! String
+            if let dictionary = identifier, dictionary.count > 1 {
+                var identifier = name
+                for key in dictionary.keys.sorted() {
+                    identifier += "-\(key):\(dictionary[key] as? String ?? "")"
+                }
+                channelUID = identifier
             }
             
             return channelUID
